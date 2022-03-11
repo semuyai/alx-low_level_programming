db0115c717b89ed00a36fa03a6ee4cbff9c349f8
semuyai <semirahayredin@yahoo.com>
2022-03-10 23:44:15 -0800

    My 6print commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description:printinggg single digits numbers of base 10 starting from 01
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 58; x <= 67; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
