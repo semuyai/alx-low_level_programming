32c1d2053c93428ce8677d15c239f8bebc1f15e8
semuyai <semirahayredin@yahoo.com>
2022-03-11 03:36:23 -0800

    My succ commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,30 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: semuyai
+ */
+
 #include <stdio.h>
+
 /**
- * main - main function
+ * main - Prints all possible combinations of single-digit numbers.
  *
- * Return: 0
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i = 0;
+	int num;
 
-	for (i = 1 ; i < 10; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar(i);
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
 		putchar(',');
+		putchar(' ');
 	}
 
 	putchar('\n');
+
 	return (0);
 }
