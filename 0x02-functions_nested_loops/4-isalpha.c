f921f56abde3e7b4ece3969f24d7cca3db7f89a0
semuyai <semirahayredin@yahoo.com>
2022-03-15 03:31:18 -0700

    My 4is commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,20 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _isalpha - checks for alphabet character
- * @c: character to check
- * Return: 1 if lower or uppercase, 0 if not alphabet character
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
-
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-	_putchar('\n');
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
