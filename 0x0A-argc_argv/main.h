1600524adac283a5af75fe6f4f074df3325ecb84
semuyai <semirahayredin@yahoo.com>
2022-04-01 00:02:05 -0700

    My main commit

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* _putchar - writes the character c to stdout
+*/
+int _putchar(char c);
+
+/**
+* print_name - prints the name of the program
+*/
+void print_name(char *s);
+
+#endif /* MAIN_H */
+
