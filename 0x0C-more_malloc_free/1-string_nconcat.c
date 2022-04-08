e14cf174af677ee49054ff9364b0c740985b9521
semuyai <semirahayredin@yahoo.com>
2022-04-08 02:46:30 -0700

    My tsk1 commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,19 +2,49 @@
 #include <stdlib.h>
 
 /**
-* malloc_checked - allocates memory using malloc
-* @b: size
+* string_nconcat - concatenates two strings
+* @s1: string 1
+* @s2: to be added to end of s1
+* @n: number of bytes to be concatenated
 *
-* Return: pointer to allocated memory
-* if malloc fails normal process termination with
-* status value of 98
+* Return: pointer to new string containg s1 followed by n bytes of s2
+* and null terminated
 */
-void *malloc_checked(unsigned int b)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	void *ptr = malloc(b);
+	char *ptr;
+	unsigned int size1 = 0, size2 = 0, i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[size1] != '\0')
+		size1++;
+
+	while (s2[size2] != '\0')
+		size2++;
+
+	ptr = malloc(sizeof(char) * (size1 + n + 1));
 
 	if (ptr == NULL)
-		exit(98);
+		return (NULL);
 
+	for (i = 0; i < size1; i++)
+		ptr[i] = s1[i];
+
+	if (n >= size2)
+		n = size2;
+
+	for (j = 0; j < n; j++)
+	{
+		ptr[i] = s2[j];
+		i++;
+	}
+
+	ptr[i] = '\0';
 	return (ptr);
 }
+
